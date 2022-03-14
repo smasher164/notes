cac078ea2ce91d10841f517a7347ebc29d5e34c9
Akhil Indurti <aindurti@gmail.com>
2022-03-14 02:14:30 -0400

    deserialize Delta with Serde

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -1,3 +1,31 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+enum Operation {
+    Insert {
+        insert: String,
+
+        #[serde(skip_serializing_if = "Option::is_none")]
+        attributes: Option<HashMap<String, serde_json::Value>>,
+    }, // TODO: data could be more than a String.
+
+    Delete {
+        delete: i64,
+    },
+    Retain {
+        retain: i64,
+
+        #[serde(skip_serializing_if = "Option::is_none")]
+        attributes: Option<HashMap<String, serde_json::Value>>,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Delta(Vec<Operation>);
+
 pub fn print_json(data: String) {
-    println!("{}", data);
+    let deserialized: Delta = serde_json::from_str(&data).unwrap();
+    println!("{:?}", deserialized);
 }
